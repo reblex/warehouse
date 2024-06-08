a752013a2f3ee43f61e5b5812601d4e7a8d503e8
Simon Wahlström <simon.otdw@gmail.com>
2024-06-08 21:52:18 +0200

    basic CLI

--- cmd/cli.go
+++ cmd/cli.go
@@ -4,9 +4,31 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		printCLIHelp()
+		return
+	}
+
+	cmd := args[0]
+	switch cmd {
+	case "ping":
+		pingCmd()
+	}
+}
+
+func printCLIHelp() {
+	fmt.Println("Use format: cmd [args]")
+	fmt.Println("ping                   - pings all services in docker to check status")
+	fmt.Println("articles <filepath>    - uploads articles from given file")
+	fmt.Println("products <filepath>    - uploads products from given file")
+}
+
+func pingCmd() {
 	pingService("http://localhost:8001/api/ping")
 	pingService("http://localhost:8002/api/ping")
 	pingService("http://localhost:8003/api/ping")
